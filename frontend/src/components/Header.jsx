340f576d452b8e2a5ac4b2f6de563a0ef23ad2cf
debasish <debasish.sahoo96@gmail.com>
2025-02-26 12:44:23 +0530

    New Update

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+function Header(){
+    return(<>
+      <div className="header">
+      <h1>Changing the World Through Design</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+      <div className="buttons">
+        <a href="#">Learn More</a>
+        <a href="#" className="hollow">Learn Less</a>
+      </div>
+    </div>
+    </>);
+}
+
+export default Header;
