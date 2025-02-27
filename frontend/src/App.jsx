a645045f9a5ca1819731c410cf7f35c51558006e
debasish <debasish.sahoo96@gmail.com>
2025-02-27 12:27:57 +0530

    Content Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,25 @@ function App() {
     title:"Changing the World Through Design",
     desc:"This is the Second Project Using React"
   }
+ const ContentData={
+  title:"Our Agency, Our Selves.",
+  desc:"This React project dynamically renders content using props and arrays. The `App.jsx` file stores data for components like **Content, Services, Stats, Gallery, and MenuLink** in structured arrays. These components use `.map()` to iterate over the data, ensuring flexibility and easy updates. By passing props, the project eliminates hardcoded values, making it scalable and efficient.",
+  imgUrl:"https://dummyimage.com/600x400/000/fff",
+  altText:"Agency Image"
+
+ }
+
+
+
+
+
+
+
   return (
     <>
       <div className="container">
         <Header data={HeaderData}/>
-        <Content />
+        <Content data={ContentData}/>
         <Services />
         <Stats />
         <hr />
