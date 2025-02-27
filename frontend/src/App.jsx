36dd8dcefdafba7d84d3d59f896aa6c6dc32d1ab
debasish <debasish.sahoo96@gmail.com>
2025-02-27 13:00:45 +0530

    New Update on Services

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,31 +7,46 @@ import MenuLink from "./components/MenuLink";
 import Stats from "./components/Stats";
 import "./App.css";
 function App() {
-  
-  const HeaderData={
-    title:"Changing the World Through Design",
-    desc:"This is the Second Project Using React"
-  }
- const ContentData={
-  title:"Our Agency, Our Selves.",
-  desc:"This React project dynamically renders content using props and arrays. The `App.jsx` file stores data for components like **Content, Services, Stats, Gallery, and MenuLink** in structured arrays. These components use `.map()` to iterate over the data, ensuring flexibility and easy updates. By passing props, the project eliminates hardcoded values, making it scalable and efficient.",
-  imgUrl:"https://dummyimage.com/600x400/000/fff",
-  altText:"Agency Image"
+  const HeaderData = {
+    title: "Changing the World Through Design",
+    desc: "This is the Second Project Using React",
+  };
+  const ContentData = {
+    title: "Our Agency, Our Selves.",
+    desc: "This React project dynamically renders content using props and arrays. The `App.jsx` file stores data for components like **Content, Services, Stats, Gallery, and MenuLink** in structured arrays. These components use `.map()` to iterate over the data, ensuring flexibility and easy updates. By passing props, the project eliminates hardcoded values, making it scalable and efficient.",
+    imgUrl: "https://dummyimage.com/600x400/000/fff",
+    altText: "Agency Image",
+  };
 
- }
-
-
-
-
-
-
+  const ServicesData = [
+    {
+      title: "Photoshop",
+      desc: "Expert photo editing and designing services.",
+    },
+    {
+      title: "Javascript",
+      desc: "Building interactive and dynamic web applications.",
+    },
+    {
+      title: "Marketing",
+      desc: "Strategic marketing solutions to boost your brand.",
+    },
+    {
+      title: "App Dev",
+      desc: "Mobile App Dev.",
+    },
+    {
+      title: "Crab Export",
+      desc: "New Crab Export",
+    },
+  ];
 
   return (
     <>
       <div className="container">
-        <Header data={HeaderData}/>
-        <Content data={ContentData}/>
-        <Services />
+        <Header data={HeaderData} />
+        <Content data={ContentData} />
+        <Services data={ServicesData}/>
         <Stats />
         <hr />
         <div className="row column">
--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,25 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Services = () => {
+const Services = ({ data }) => {
   return (
     <div className="services">
-      <div className="service">
-        <h3>Photoshop</h3>
-        <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam
-          eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
-      </div>
-      <div className="service">
-        <h3>Javascript</h3>
-        <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam
-          eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
-      </div>
-      <div className="service">
-        <h3>Marketing</h3>
-        <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam
-          eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
-      </div>
+      {data.map((element, index) => (
+        <div className="service" key={index}>
+          <h3>{element.title}</h3>
+          <p>{element.desc}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
