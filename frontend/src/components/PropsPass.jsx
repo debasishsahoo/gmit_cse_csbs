49152969ae6ebfddcc6602eebbefc70c7840e088
debasish <debasish.sahoo96@gmail.com>
2025-02-28 12:29:40 +0530

    PropsPass

--- frontend/src/components/PropsPass.jsx
+++ frontend/src/components/PropsPass.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const PropsPass = () => {
+const PropsPass = ({ persons }) => {
   return (
     <div>
-      
+      {
+                persons.map((person, index) => {
+                    return <h1 key={index}>Value = {person} | Key = {index}</h1>
+                })
+            }
     </div>
   )
 }
