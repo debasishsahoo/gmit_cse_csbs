380d8802de08587b3705422d656e4d4e5cb2c751
debasish <debasish.sahoo96@gmail.com>
2025-02-28 12:06:58 +0530

    PropsPasss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import PropsBasic from "./components/PropsBasic";
 import ValueProps from "./components/ValueProps";
+import PropsPass from "./components/PropsPass";
 import "./App.css";
 function App() {
-  let anyValue = "JAVASCRIPT"+"2.0"
+  let anyValue = "JAVASCRIPT" + "2.0";
   return (
     <>
       <PropsBasic name="Debasish" title="Sahoo" />
       <ValueProps
-      strData="this is String Value"
-      numData={123}
-      boolData={false}
-      objData={{name:"Deb",title:"Sahoo"}}
-      arrData={[1,2,3,4,5,6,7,8]}
-      jsxData={2+2+4+8-3}
-      anyData={anyValue}
+        strData="this is String Value"
+        numData={123}
+        boolData={false}
+        objData={{ name: "Deb", title: "Sahoo" }}
+        arrData={[1, 2, 3, 4, 5, 6, 7, 8]}
+        jsxData={2 + 2 + 4 + 8 - 3}
+        anyData={anyValue}
       />
+
+      <PropsPass persons={["Archisman", "Sourav", "Jain", "treema"]} />
     </>
   );
 }
---
+++ frontend/src/components/PropsPass.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropsPass = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PropsPass
