5f4a0eda31ce7f178294bd923668098350837fde
debasish <debasish.sahoo96@gmail.com>
2025-02-28 12:48:47 +0530

    Spraed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import PropsBasic from "./components/PropsBasic";
 import ValueProps from "./components/ValueProps";
 import PropsPass from "./components/PropsPass";
+import PropsSpread from "./components/PropsSpread";
 import "./App.css";
 function App() {
   let anyValue = "JAVASCRIPT" + "2.0";
+  const SpreadData = { stack1: "REACT", stack2: "ANGULAR" ,stack3:"NEXTJS",stack4:"TYPESCRIPT"};
   return (
     <>
       <PropsBasic name="Debasish" title="Sahoo" />
@@ -18,6 +20,11 @@ function App() {
       />
 
       <PropsPass persons={["Archisman", "Sourav", "Jain", "treema"]} />
+      <PropsSpread 
+      a={SpreadData.stack1} 
+      b={SpreadData.stack4}/>
+      <br/>
+      <PropsSpread {...SpreadData}/>
     </>
   );
 }
---
+++ frontend/src/components/PropsSpread.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropsSpread = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PropsSpread
