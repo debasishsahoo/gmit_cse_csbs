f8f02087a8c1e4f9161bc4a6766ec755c258c269
debasish <debasish.sahoo96@gmail.com>
2025-03-11 11:23:11 +0530

    New Update

---
+++ frontend/src-3/App.jsx
@@ -0,0 +1,39 @@
+// import PropsBasic from "./components/PropsBasic";
+// import ValueProps from "./components/ValueProps";
+// import PropsPass from "./components/PropsPass";
+// import PropsSpread from "./components/PropsSpread";
+import Child from './components/Child'
+import Parent from './components/Parent'
+import "./App.css";
+function App() {
+  let anyValue = "JAVASCRIPT" + "2.0";
+  // const SpreadData = { stack1: "REACT", stack2: "ANGULAR" ,stack3:"NEXTJS",stack4:"TYPESCRIPT"};
+  return (
+    <>
+      {/* <PropsBasic name="Debasish" title="Sahoo" />
+      <ValueProps
+        strData="this is String Value"
+        numData={123}
+        boolData={false}
+        objData={{ name: "Deb", title: "Sahoo" }}
+        arrData={[1, 2, 3, 4, 5, 6, 7, 8]}
+        jsxData={2 + 2 + 4 + 8 - 3}
+        anyData={anyValue}
+      />
+
+      <PropsPass persons={["Archisman", "Sourav", "Jain", "treema"]} /> 
+      <PropsSpread 
+      a={SpreadData.stack1} 
+      b={SpreadData.stack4}/>
+      <br/>
+      <p>------------------------------</p>
+      <PropsSpread {...SpreadData}/>*/}
+
+      <Parent>
+        <Child a={anyValue}/>
+      </Parent>
+    </>
+  );
+}
+
+export default App;
