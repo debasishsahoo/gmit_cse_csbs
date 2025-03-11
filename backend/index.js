03bf2fc72d7a7c05877b25e30f91748c0ac35030
debasish <debasish.sahoo96@gmail.com>
2025-03-11 13:30:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); // Import CORS
 const connectDB = require("./configs/db.mongo.conn");
 require("dotenv").config();
 const userRouter=require("./routers/user.route")
@@ -10,6 +11,15 @@ const HOST = process.env.HOST;
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+// ✅ Enable CORS
+server.use(
+  cors({
+    origin: "*", // Allow frontend to access the API
+    //origin: "http://localhost:5173", //Production  Value
+    credentials: true, // Allow cookies & authentication headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  })
+);
 
 server.use("/api/health", (req, res) => {
   res.status(200).json({
