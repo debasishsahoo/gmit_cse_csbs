29cdb89287bdd1ed3eb5f4d7e7081fc5fcfd3e78
debasish <debasish.sahoo96@gmail.com>
2025-03-12 12:14:01 +0530

    Update user.route.js

--- backend/routers/user.route.js
+++ backend/routers/user.route.js
@@ -3,6 +3,13 @@ const userController = require("../controllers/user.controller");
 
 
 router.post("/signup", userController.SIGNUP);
+router.post("/signin", userController.SIGNIN);
+router.post("/signout", userController.SIGNOUT);
+router.put("/change-password", userController.CHANGE_PASSWORD);
+router.get("/:id", userController.GET_USER_DETAILS);
+router.put("/:id", userController.UPDATE_USER_PROFILE);
+router.delete("/:id", userController.DELETE_USER_PROFILE);
+
 
 
 module.exports = router;
